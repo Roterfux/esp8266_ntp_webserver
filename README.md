6e1092ec931fa0e94cfb29bf238cf865d00959fa
RoterFux <jgre@vivaldi.net>
2018-12-04 23:50:48 +0100

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # esp8266_ntp_webserver
++ LED support
++ NTP time sync
++ Time persistance
++ Very basic Webserver
--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def do_connect():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('Fuxbau', 'JuV30062013')
+        sta_if.connect('-', '-')
         while not sta_if.isconnected():
             print(".", end="")
             time.sleep(.25)
